dc11d7e79641d70765c73d1718cff9c9555a9524
osaless <137958294+osaless@users.noreply.github.com>
2023-07-06 20:56:57 -0300

    Add files via upload

---
+++ 06-07/script.js
@@ -0,0 +1,13 @@
+var nome = document.getElementById("nome")
+var email = document.getElementById("email")
+var telefone = document.getElementById("tel")
+var profissao = document.getElementById("profissao")
+
+
+function salvar() {
+    if(s){ 
+
+
+    }
+    
+}
